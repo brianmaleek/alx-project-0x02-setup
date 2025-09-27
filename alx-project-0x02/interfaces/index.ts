d8844b04830255ba60982903e951a7776a558a31
Brian Maleek <maleekmalik@gmail.com>
2025-09-27 21:18:28 +0300

    feat: add UserProps and UsersPageProps interfaces for user data management

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -39,3 +39,31 @@ export interface PostProps {
 export interface PostPageProps {
     posts: PostProps[];
 }
+
+export interface UserProps {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
+}
+
+export interface UsersPageProps {
+    users: UserProps[];
+}
