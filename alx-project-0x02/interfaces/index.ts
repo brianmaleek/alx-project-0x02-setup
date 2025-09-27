28e9fd968813c370ca525d0e7d2594b26184e2ae
Brian Maleek <maleekmalik@gmail.com>
2025-09-27 04:03:26 +0300

    feat: add ButtonProps interface for button customization options

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -9,3 +9,11 @@ export interface PostModalProps {
     onClose: () => void;
     onAddPost: (title: string, content: string) => void;
 }
+
+export interface ButtonProps {
+    label: string;
+    size?: "small" | "medium" | "large";
+    shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+    onClick?: () => void;
+    className?: string;
+}
